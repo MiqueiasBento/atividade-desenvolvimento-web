bbe15bfa38f68051fcdf284962195a4e5baaefc0
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-06-24 00:16:09 -0300

    config: autorizacao de acesso do CORS da API

--- integracao-e-requisicao/api/app.js
+++ integracao-e-requisicao/api/app.js
@@ -9,15 +9,27 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const cors = require('cors')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// Middleware de CORS
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+})
 
 // Importa a rota
 const alunoRouter = require('./routes/aluno.route');
@@ -46,12 +58,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-})
 
 module.exports = app;
