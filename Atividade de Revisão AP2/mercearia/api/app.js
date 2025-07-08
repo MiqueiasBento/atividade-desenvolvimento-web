bbea5f4773177abdc9df1885bcd8dbd23ceafe08
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-07-08 08:32:39 -0300

    feat: adicionando permissão de cors

--- Atividade de Revisão AP2/mercearia/api/app.js
+++ Atividade de Revisão AP2/mercearia/api/app.js
@@ -9,10 +9,13 @@ var produtosRouter = require('./routes/produto.route');
 
 var app = express();
 
+const cors = require('cors');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
