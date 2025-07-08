d7d433dae45687512806e0afd46f010eb48c0731
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-07-08 09:29:38 -0300

    feat: adicionando destaque por media

--- Atividade de Revisão AP2/mercearia/interface/script.js
+++ Atividade de Revisão AP2/mercearia/interface/script.js
@@ -3,9 +3,18 @@ async function buscarProdutos() {
         const response = await fetch("http://localhost:3000/produtos");
         const dados = await response.json();
 
+        let media = 0;
+        dados.forEach(produto => {
+            media += produto.preco;
+        });
+        media /= dados.length;
+
+        const mediaElement = document.getElementById('media-produtos');
+        mediaElement.textContent = `Media do preço dos produtos: R$ ${media.toFixed(2)}`;
+
         const tabela = document.getElementById('tabela-produtos');
         tabela.innerHTML = '';
-        
+
         dados.forEach(produto => {
             const linha = document.createElement('tr');
             linha.innerHTML = 
@@ -17,6 +26,10 @@ async function buscarProdutos() {
                 <td>${produto.descricao}</td>
                 <td>${produto.quantidade}</td>
                 `;
+
+            if(produto.preco > media) {
+                linha.style.backgroundColor = 'lightcoral';
+            }
 
             tabela.appendChild(linha);
         });
